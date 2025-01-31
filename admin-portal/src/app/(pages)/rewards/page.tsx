b0520a4a04fee076ca9ee81cbd4a1b8ab9c02f2f
danielkao14 <daniel.kao196@gmail.com>
2025-01-30 21:02:10 -0800

    Create Rewards page

---
+++ admin-portal/src/app/(pages)/rewards/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+// Added following line
+import { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import OrganizationProfile from "@/app/_components/OrganizationProfile";
+
+export default function Rewards() {
+  const [responseValue, setResponseValue] = useState();
+
+  // Example of requesting data from the database/backend and making use of the data.
+  const exampleGetToRoot = async () => {
+    try {
+      const response: AxiosResponse = await axios.get(`http://${process.env.IP_ADDRESS}:${process.env.PORT}/transactions/`);
+      setResponseValue(response.data);
+    } catch (error) {
+      console.log(error);
+    } // Post request
+  }
+
+  return (
+    <div>
+     Rewards
+     <button onClick={() => { console.log("Result: ", exampleGetToRoot()) }}>
+        Example HTTP Request button
+      </button>
+      <div>
+        Result: {responseValue}
+      </div>
+      <OrganizationProfile></OrganizationProfile>
+    </div>
+  );
+}
