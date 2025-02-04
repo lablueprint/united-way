26f124ac2ce78cc70e2d55055cdb9e2226bd1168
whol3wh3at <97588267+whol3wh3at@users.noreply.github.com>
2025-02-03 22:56:55 -0800

    events populated! w ccaroline

--- mobile/app/_components/Events.tsx
+++ mobile/app/_components/Events.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import { View, Button, Text, StyleSheet} from 'react-native';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const styles = StyleSheet.create({
     titletext: {
@@ -8,16 +8,79 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingLeft: '8%',
     color: "white",
+  },
+
+  eventsTitle: {
+    fontSize: 20,
+    paddingTop: 5,
+    paddingLeft: '8%',
+    color: "white",
+  },
+
+  eventsDate: {
+    fontSize: 15,
+    paddingTop: 5,
+    paddingLeft: '8%',
+    color: "white",
   }
 });
 
 export default function Events() {
+
+  const [allEvents, setAllEvents] = useState([])
+
+  const getEvents = async () => {
+    console.log("hi caroline");
+    try {
+      const events: AxiosResponse = await axios.get(`http://${process.env.EXPO_PUBLIC_SERVER_IP}:${process.env.EXPO_PUBLIC_SERVER_PORT}/events/`)
+      const {data} = events.data
+      return data
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+  
+    const fetchEvents  = async () => {
+      const data = await getEvents();
+      setAllEvents(data)
+    }; 
     
+    fetchEvents();
+        
+    console.log("hi angela")
+
+    }, []);
+
+    const fetchEvents  = async () => {
+      const data = await getEvents();
+      setAllEvents(data)
+    }; 
+
+    // console.log(allEvents[0])
     return (
       <View>
         <Text style={styles.titletext}>Events</Text>
+
+
+        {
+          allEvents.length !== 0 ?
+          allEvents.map((events) => {
+            console.log(typeof events)
+            console.log(events)
+            return(
+              <View>
+                <Text style={styles.eventsTitle}> {events.name} </Text>
+              <Text style={styles.eventsDate}>{events.date}</Text>
+              </View>
+            )
+          }) :
+          <></>
+        }
+
       </View>
     );
+
   }
-
-
