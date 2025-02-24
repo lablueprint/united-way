4a5a6311914c9a6b51db889c5f6d9662b2f47442
Edward Ng <ehng359@g.ucla.edu>
2025-02-24 14:17:31 -0800

    Minor changes to handle more robust persistence upon refresh

--- admin-portal/next.config.ts
+++ admin-portal/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    IP_ADDRESS: "164.67.215.208",
-    PORT: "4000",
+    IP_ADDRESS: "",
+    PORT: "",
   }
 };
 
