22e8a008f723f0e19a69e085240e74eca0169ae6
Edward Ng <ehng359@g.ucla.edu>
2025-02-27 19:42:28 -0800

    Removed specific IP from repository

--- admin-portal/next.config.ts
+++ admin-portal/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    IP_ADDRESS: "172.26.60.162",
-    PORT: "4000",
+    IP_ADDRESS: "",
+    PORT: "",
   },
 };
 
