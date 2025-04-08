50f14498c8b0129718a604ea3ffa7ad4479590ea
JasonChoyCode <86578475+JasonChoyCode@users.noreply.github.com>
2025-04-07 23:05:44 -0700

    new aws implementations

--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
-require('dotenv').config(); // populates all 'secrets' in everything we defined in .env file (how we access ports and other info)
+require("dotenv").config(); // populates all 'secrets' in everything we defined in .env file (how we access ports and other info)
 
 // Module Imports
-const express = require('express'); //define route thru express 
+const express = require("express"); //define route thru express
 const { expressjwt: jwt } = require("express-jwt");
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT;
 
 // Route Imports
-const exampleRouter = require('./routes/exampleRoute.js');
-const authRouter = require('./routes/authRoutes.js')
-const eventRouter = require('./routes/eventRoutes.js');
-const organizationRouter = require('./routes/organizationRoutes.js');
-const userRouter = require('./routes/userRoutes.js');
-const activityRouter = require('./routes/activityRoutes.js')
+const imageRouter = require("./routes/imageRoutes.js"); // import image router from routes folder
+const exampleRouter = require("./routes/exampleRoute.js");
+const authRouter = require("./routes/authRoutes.js");
+const eventRouter = require("./routes/eventRoutes.js");
+const organizationRouter = require("./routes/organizationRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
+const activityRouter = require("./routes/activityRoutes.js");
 
 // Connect to the MongoDB database
 async function connectToDatabase() {
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 }
 
@@ -32,43 +37,45 @@ connectToDatabase();
 // Start the Node Express server
 const app = express(); //define app using express, defines handlers
 app.use(cors()); // use app.use to use router -- cross origin requests, allow retrieve req from diff ip address
-app.use(express.json()); 
+app.use(express.json());
 
 // API Routes
-app.use('/test', exampleRouter); // given ip address, /test is where example router logic will be handle
-app.use('/auth', authRouter);
+app.use("/test", exampleRouter); // given ip address, /test is where example router logic will be handle
+app.use("/auth", authRouter);
 
-app.use('/orgs', 
-  jwt(
-  {
-    secret: process.env.JWT_SECRET, 
-    algorithms: ["HS256"]
-  }).unless({path: ["/orgs/createOrg", "/orgs/filtered"]})
-)
-app.use('/orgs', organizationRouter);
+app.use("/images;", imageRouter);
+app.use(
+  "/orgs",
+  jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+  }).unless({ path: ["/orgs/createOrg", "/orgs/filtered"] })
+);
+app.use("/orgs", organizationRouter);
 
-app.use('/users', 
-  jwt(
-  {
-    secret: process.env.JWT_SECRET, 
-    algorithms: ["HS256"]
-  }).unless({path: ["/users/createUser", /^\/users\/email\/([^\/]*)$/]})
-)
-app.use('/users', userRouter);
+app.use(
+  "/users",
+  jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+  }).unless({ path: ["/users/createUser", /^\/users\/email\/([^\/]*)$/] })
+);
+app.use("/users", userRouter);
 
-app.use('/events',
-  jwt (
-    {
-      secret: process.env.JWT_SECRET, 
-      algorithms: ["HS256"]
-    })
-)
-app.use('/events', eventRouter);
+app.use(
+  "/events",
+  jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+  })
+);
+app.use("/events", eventRouter);
 
-app.use('/activities', activityRouter);
+app.use("/activities", activityRouter);
 
-app.get('/', (req, res) => { // defines a route where if we send get req to the route, will send back resp
-  res.send('Hello World!'); //routers are groupings of endpoints
+app.get("/", (req, res) => {
+  // defines a route where if we send get req to the route, will send back resp
+  res.send("Hello World!"); //routers are groupings of endpoints
 });
 
 app.listen(port, () => {
