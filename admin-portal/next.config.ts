3628cec951152a35d8a8cd67ae7d3f03dc48e4fb
Eric Lu <84652493+ericslu@users.noreply.github.com>
2025-04-21 23:00:39 -0700

    Update local branch from event_creation

--- admin-portal/next.config.ts
+++ admin-portal/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    IP_ADDRESS: "",
-    PORT: "",
+    IP_ADDRESS: "172.26.80.90",
+    PORT: "4000",
   }
 };
 
