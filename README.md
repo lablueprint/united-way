5b80c87ca5635c090b19bc62e58bb22c05e7ec4e
Edward Ng <ehng359@g.ucla.edu>
2025-05-18 20:19:27 -0700

    Rename components + migrate to useApiAuth

--- README.md
+++ README.md
@@ -1,10 +1,45 @@
 # United Way
-24-25 Full-Stack Web & Mobile Project
+24-25 Full-Stack Web & Mobile Project in collaboration with United Way of Greater Los Angeles
 
-Installation
-Change directories into the repository `united-way`. Run the command `cd admin-portal && npm i && cd ../mobile && npm i && cd ../server && npm i`. This will directly install the requisite dependencies that are listed within `package.json` to be able to run the web and mobile applications, respectively.
+## Objective
+![image](https://github.com/user-attachments/assets/58f435e5-1943-44dc-8283-fbb0618ed0c8)
 
-Running
-You should create two terminals, one based in the mobile directory AND/OR web directory (depending on the sprint being worked on) and one based in the server directory for running the server.
+Our project builds on the mission to make it easier for residents to connect with small businesses, discover local events, track participation, and earn rewards through a mobile platform, and for community organizers to publish their events on a web application.
 
-Then, run npm start in the server terminal and run npx expo start in the mobile terminal. Use the Expo Go App and the QR code to run the code on your mobile device.
+## Installation
+Change directories into the repository `united-way`. 
+Run the command:
+```shell
+cd admin-portal && npm i && cd ../mobile && npm i && cd ../server && npm i`
+```
+This will directly install the requisite dependencies that are listed within `package.json` to be able to run the web, mobile and server applications, respectively.
+
+## Running web
+```shell
+  # Installing dependencies
+  cd admin-portal
+  npm i
+  # Running the development build
+  npm run dev
+  # OR
+  # Running a production build
+  npx next build
+  npm start
+```
+
+## Running mobile
+```shell
+  cd mobile
+  npm i
+  # Running the Expo Go
+  npx expo start --tunnel
+  # OR
+  # Running on the iOS Simulator
+  npx expo start
+```
+
+To be able to run any of the following web or mobile functionality, the server must be run with:
+```shell
+  cd server
+  node index.js
+```
--- admin-portal/src/app/(pages)/landing/page.tsx
+++ admin-portal/src/app/(pages)/landing/page.tsx
@@ -5,17 +5,15 @@ import { useRouter } from 'next/navigation';
 import EventCard from "@/app/_components/EventCard";
 import EventEditor from "@/app/_components/EventEditor";
 import EventCarousel from "@/app/_components/EventCarousel";
-import NoMoreEventsTrigger from "@/app/_components/NoMoreEvents";
-import useApiAuth from '@/app/_hooks/useApiAuth';
-import { RequestType } from '@/app/_interfaces/RequestInterfaces';
-import { UseDispatch } from 'react-redux';
-import { EventData } from '@/app/_interfaces/EventInterfaces';
-import { refresh } from '../../_utils/redux/orgSlice';
+import EventEndMarker from '@/app/_components/EventEndMarker';
+
 import Image from "next/image";
 import { emptyLogo, addIcon, rightArrow, single, attendee } from '../../../../public/Landing/Landing-index';
 
 import { useState, useEffect } from "react";
-import axios, { AxiosResponse } from 'axios';
+
+import useApiAuth from '@/app/_hooks/useApiAuth';
+import { RequestType } from '@/app/_interfaces/RequestInterfaces';
 import styles from "./page.module.css"
 
 // TO-DO:
@@ -66,8 +64,8 @@ export default function Landing() {
     date: new Date(),
     description: "",
     location: {
-        type: "",
-        coordinates: [],
+      type: "",
+      coordinates: [],
     },
     tags: [],
     registeredUsers: [],
@@ -82,105 +80,102 @@ export default function Landing() {
   };
 
 
-  const isToday = ( inputDate : Date ) => {
+  const isToday = (inputDate: Date) => {
     const today = new Date();
 
     return ((inputDate.getDate() == today.getDate()) &&
-       (inputDate.getMonth() == today.getMonth()) &&
-       (inputDate.getFullYear() == today.getFullYear()));
+      (inputDate.getMonth() == today.getMonth()) &&
+      (inputDate.getFullYear() == today.getFullYear()));
   }
 
-  const isUpcoming = ( inputDate : Date ) => {
+  const isUpcoming = (inputDate: Date) => {
     const today = new Date();
-    today.setHours(0,0,0,0);
+    today.setHours(0, 0, 0, 0);
 
     const copyInput = new Date(inputDate); //prevent mutating original 
-    copyInput.setHours(0,0,0,0);
-    
+    copyInput.setHours(0, 0, 0, 0);
+
     return copyInput >= today;
   }
 
   const getEventById = async (id: string) => {
     try {
-        const response: AxiosResponse = await axios.get(`http://${process.env.IP_ADDRESS}:${process.env.PORT}/events/${id}`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${org.authToken}`
-            }
-        });
-        const { data } = response.data;
-        return data;
+      const body = {};
+      const endpoint = `events/${id}`;
+      const requestType = RequestType.GET;
+      const data = await sendRequest({ body, endpoint, requestType });
+      return data;
     } catch (err) {
-        console.log(err);
-        return err;
+      console.log(err);
+      return err;
     }
-};
+  };
 
-const createBlankEvent = async () => {
-  try {
-    const requestType = RequestType.POST;
-    const endpoint = "events/orgs/:id/createEvent";
-    const body = {
-      name: "Your Event Name",
-      date: new Date(),
-      duration: 0, // Hardcoded for now
-      draft: true,
-      draftList: [],
-      description: "Your Event Description",
-      startTime: '12:00',
-      endTime: '12:01',
-      location: {
-        type: "Point",
-        coordinates: [0, 0]
-      },
-      organizerID: org.orgId,
-      tags: [],
-      registeredUsers: [], // Hardcoded for now
-      activity: [], // Hardcoded for now
-      image: "placeholder" // Hardcoded for now
+  const createBlankEvent = async () => {
+    try {
+      const requestType = RequestType.POST;
+      const endpoint = "events/orgs/:id/createEvent";
+      const body = {
+        name: "Your Event Name",
+        date: new Date(),
+        duration: 0, // Hardcoded for now
+        draft: true,
+        draftList: [],
+        description: "Your Event Description",
+        startTime: '12:00',
+        endTime: '12:01',
+        location: {
+          type: "Point",
+          coordinates: [0, 0]
+        },
+        organizerID: org.orgId,
+        tags: [],
+        registeredUsers: [], // Hardcoded for now
+        activity: [], // Hardcoded for now
+        image: "placeholder" // Hardcoded for now
+      };
+      const data = await sendRequest({ requestType, endpoint, body });
+      return data._id;
+    } catch (err) {
+      console.log(err);
+      return ""
+    }
+  }
+
+  useEffect(() => {
+    const fetchTodayEvent = async () => {
+      if (todayIds.length === 1) {
+        const data = await getEventById(todayIds[0]);
+        if (data) {
+          setEventData({
+            ...data,
+            date: new Date(data.date)
+          });
+        }
+      }
     };
-    const data = await sendRequest({ requestType, endpoint, body });
-    return data._id;
-  } catch (err) {
-    console.log(err);
-    return ""
-  }
-}
+    fetchTodayEvent();
+  }, [todayIds]);
 
-useEffect(() => {
-  const fetchTodayEvent = async () => {
-    if (todayIds.length === 1) {
-      const data = await getEventById(todayIds[0]);
-      if (data) {
-        setEventData({
-          ...data,
-          date: new Date(data.date)
-        });
+  useEffect(() => {
+    const fetchAllTodayEvents = async () => {
+      const events: EventData[] = [];
+      for (const id of todayIds) {
+        const data = await getEventById(id);
+        if (data) {
+          events.push({ ...data, date: new Date(data.date) });
+        }
       }
+
+      setAllEvents(events);
+    };
+
+    if (todayIds.length > 0) {
+      fetchAllTodayEvents();
     }
-  };
-  fetchTodayEvent();
-}, [todayIds]);
+  }, [todayIds]);
 
-useEffect(() => {
-  const fetchAllTodayEvents = async () => {
-    const events: EventData[] = [];
-    for (const id of todayIds) {
-      const data = await getEventById(id);
-      if (data) {
-        events.push({ ...data, date: new Date(data.date) });
-      }
-    }
-
-    setAllEvents(events);
-  };
-
-  if (todayIds.length > 0) {
-    fetchAllTodayEvents();
-  }
-}, [todayIds]);
-
-useEffect(() => {
+  useEffect(() => {
     const getOrganizerEvents = async () => {
       try {
         const requestType = RequestType.POST;
@@ -191,7 +186,7 @@ useEffect(() => {
         };
         const data = await sendRequest({ requestType, body, endpoint })
         console.log('data', data)
-        const normalizedEventData = data.map((event : EventData) => ({
+        const normalizedEventData = data.map((event: EventData) => ({
           ...event,
           date: new Date(event.date),
         }))
@@ -209,26 +204,26 @@ useEffect(() => {
         console.log(err);
       }
     }
-    
+
     getOrganizerEvents();
   }, []);
 
   return (
     <div className={styles.body}>
-      <div className={styles.navBar}> 
+      <div className={styles.navBar}>
         <button className={styles.button} onClick={() => { dispatchLogout(); router.push('/sign-up'); }}>
           Log out
         </button>
       </div>
-      <div className={styles.hero}>      
+      <div className={styles.hero}>
         <div className={styles.heroInfo}>
           <div className={styles.heroHeader}>
             <h1 className={styles.heroWelcome}>Welcome, {orgName}</h1>
             <p className={styles.heroText}>This is your admin portal. You’re able to manage events and rewards for users.</p>
           </div>
-          <button 
+          <button
             className={styles.button}
-            onClick={ async () => { 
+            onClick={async () => {
               const _id = await createBlankEvent()
 
               if (_id != "") {
@@ -236,85 +231,85 @@ useEffect(() => {
                 setEditingId(_id);
               }
             }}>
-            <Image src={addIcon} alt="Plus icon for creating a new event" width={10}/>
+            <Image src={addIcon} alt="Plus icon for creating a new event" width={10} />
             {isEditing ? "Cancel Event" : "Create Event"}
           </button>
         </div>
       </div>
-      { upcomingIds.length > 0 ? (
+      {upcomingIds.length > 0 ? (
         <div className={styles.content}>
           <div className={styles.eventContainer}>
             {/* If no events today, display empty event screen. */}
-            { todayIds.length > 0 ? (
+            {todayIds.length > 0 ? (
               <div>
                 <div className="event-list today-list">
                   {/* single event view*/}
-                  { todayIds.length == 1 ? (
-                      <div className="single event-today">
-                          <Image className="single-event-img" src={single} alt="Event thumbnail"/>
-                          <div className="event-info">
-                            <div className="event-date-time">
-                              <p className="event-date">Today</p>
-                              <p className="event-time">{startTime} - {endTime}</p>
-                            </div>
-                            <div className="event-name">{eventData.name}</div>
-                            <div className="event-details">
-                            <div className="event-location">{location}</div>
-                              <div className="event-attendees">
-                                <Image src={attendee} alt="Attendee icon" />
-                                <p className="attendee-info">{eventData.registeredUsers.length} Attendees</p>
-                              </div>
-                            </div>
+                  {todayIds.length == 1 ? (
+                    <div className="single event-today">
+                      <Image className="single-event-img" src={single} alt="Event thumbnail" />
+                      <div className="event-info">
+                        <div className="event-date-time">
+                          <p className="event-date">Today</p>
+                          <p className="event-time">{startTime} - {endTime}</p>
+                        </div>
+                        <div className="event-name">{eventData.name}</div>
+                        <div className="event-details">
+                          <div className="event-location">{location}</div>
+                          <div className="event-attendees">
+                            <Image src={attendee} alt="Attendee icon" />
+                            <p className="attendee-info">{eventData.registeredUsers.length} Attendees</p>
+                          </div>
                         </div>
                       </div>
+                    </div>
 
                   ) : (
                     <EventCarousel events={allEvents} intervalMs={2000} visibleCount={4} />
                   )}
                 </div>
               </div>
-            ) : ( 
+            ) : (
               <div className={styles.emptyList}>
                 <div className={styles.emptyListContainer}>
                   <h2 className={styles.emptyTitle}>Events Today</h2>
                   <div className={styles.emptyContentContainer}>
                     <div className={styles.emptyImgContainer}>
-                      <Image width={80} src={emptyLogo} alt="United Way Logo Sheer Blue"/>
+                      <Image width={80} src={emptyLogo} alt="United Way Logo Sheer Blue" />
                     </div>
                     <p className={styles.emptyText}>No events today.</p>
                   </div>
                 </div>
               </div>
-            )} 
+            )}
           </div>
           <div className={styles.eventContainer}>
             <div className={styles.listTitle}>
               <h2 className={styles.subtitle}>Upcoming Events</h2>
-              <h2 className={styles.subtitleLink}>View All Events <Image className="arrow-icon" width={11} src={rightArrow} alt="Right Arrow Icon"/></h2>
+              <h2 className={styles.subtitleLink}>View All Events <Image className="arrow-icon" width={11} src={rightArrow} alt="Right Arrow Icon" /></h2>
             </div>
             <div className={`${styles.eventList} ${styles.upcomingList}`}>
-              { upcomingIds.length > 0  && upcomingIds.map((id: string) => {
+              {upcomingIds.length > 0 && upcomingIds.map((id: string) => {
                 return (
                   <EventCard id={id} key={id} removeFromList={removeFromList} orgName={orgName} />
                 );
               })}
             </div>
           </div>
-          <NoMoreEventsTrigger />
+          <EventEndMarker />
         </div>
-        
-        ) : (
-          <div className={styles.emptyEvent}>
-            <div className={styles.emptyContent}>
-              <Image src={emptyLogo} alt="United Way Sheer Blue Logo" width={80}/>
-              <p className={styles.emptyText}>No upcoming events.</p>
-            </div>
-          </div>
 
-        )}
-            
-            {isEditing && <EventEditor orgName={orgName} changeState={setIsEditing} eventId={editingId} justCreated={true} />}
+      ) : (
+        <div className={styles.emptyEvent}>
+          <div className={styles.emptyContent}>
+            <Image src={emptyLogo} alt="United Way Sheer Blue Logo" width={80} />
+            <p className={styles.emptyText}>No upcoming events.</p>
+          </div>
+        </div>
+
+      )}
+
+      {isEditing && <EventEditor orgName={orgName} changeState={setIsEditing} eventId={editingId} justCreated={true} />}
     </div>
   );
-  
+
 }
