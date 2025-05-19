41e150e4cd3926e38ff251ce5e199273f12d3704
Edward Ng <ehng359@g.ucla.edu>
2025-05-18 21:14:17 -0700

    Minor edits before merge

--- admin-portal/next.config.ts
+++ admin-portal/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    IP_ADDRESS: "192.168.1.218",
-    PORT: "4000",
+    IP_ADDRESS: "",
+    PORT: "",
   }
 };
 
