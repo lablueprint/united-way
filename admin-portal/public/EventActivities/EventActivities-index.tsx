930668e22c408bd3a9391a9f3ec593e04747660c
Eric Lu <84652493+ericslu@users.noreply.github.com>
2025-05-18 19:52:58 -0700

    Add dynamic display of announcement details - Add EventActivity components to allow for easy construction of new activity dropdown menus - match the hifi still as per the usual - add new changes from figma to event creation portion - add svg files to support new activities

---
+++ admin-portal/public/EventActivities/EventActivities-index.tsx
@@ -0,0 +1,11 @@
+import alert from './images/alert.svg'
+import drop_down from './images/drop_down.svg'
+import drop_up from './images/drop_up.svg'
+import menu from './images/menu.svg'
+
+export {
+    alert,
+    drop_down,
+    drop_up,
+    menu,
+}
---
+++ admin-portal/src/app/_components/EventActivityProps/EventActivityDropdown.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../_interfaces/AuthInterfaces';
+import { Activity } from "../../_interfaces/EventInterfaces";
+import Image from 'next/image'
+import { drop_down, drop_up } from '../../../../public/EventActivities/EventActivities-index'
+import ActivityCard from "./ActivityCard";
+import '../../_styles/EventActivityDropDown.css';
+
+interface EventActivityDropDownProps {
+    activityTitle: string;
+    activities: Activity[];
+    setActivities: React.Dispatch<React.SetStateAction<Activity[]>>;
+    isExpanded: boolean;
+    setIsExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+    ContentComponent: React.ComponentType<any>;
+}
+
+export default function EventActivityDropdown({ activityTitle, activities, setActivities, isExpanded, setIsExpanded, ContentComponent }: EventActivityDropDownProps) {
+    return (
+        isExpanded ?
+        <div className="dropdown-parent">
+            <div className="activity-grandparent">
+                <div className="activity-header-row">
+                    <div className="activity-title-count-parent">
+                        <div className="activity-title-text">{activityTitle}</div>
+                        <div className="activity-count">
+                            {activities.length}
+                        </div>
+                    </div>
+                    <div className="activity-button-dropdown">
+                        <div className="add-button">
+                            + ADD
+                        </div>
+                        <div className="drop_down" onClick={() => {setIsExpanded(false)}}>
+                            <Image src={drop_up} alt="Drop Up Icon" width={18} height={42} />
+                        </div>
+                    </div>
+                </div>
+                <div className="activity-card-container">
+                    {activities.map((a) => (
+                        <div key={a._id}>
+                            <ActivityCard 
+                                activity={a} 
+                                ContentComponent={ContentComponent}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="divider"/>
+        </div>
+        :
+        <div className="dropdown-parent">
+            <div className="activity-grandparent">
+                <div className="activity-header-row">
+                    <div className="activity-title-count-parent">
+                        <div className="activity-title-text">{activityTitle}</div>
+                        <div className="activity-count">
+                            {activities.length}
+                        </div>
+                    </div>
+                    <div className="activity-button-dropdown">
+                        <div className="add-button">
+                            + ADD
+                        </div>
+                        <div className="drop_down" onClick={() => {setIsExpanded(true)}}>
+                            <Image src={drop_down} alt="Drop Down Icon" width={18} height={42} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="divider"/>
+        </div>
+    )
+}
