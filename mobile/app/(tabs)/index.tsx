17041a63f86b117e0f4a33a32b1af1898bd9e553
Edward Ng <ehng359@g.ucla.edu>
2025-05-19 17:22:10 -0700

    Fix main error

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,7 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { StyleSheet, View, Text, ScrollView, TouchableOpacity, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
-import { useLocalSearchParams, useRouter } from 'expo-router';
 import WeekCalendar from '../_components/WeekCalendar';
 
 const styles = StyleSheet.create({
@@ -22,15 +20,6 @@ const styles = StyleSheet.create({
 });
 
 //get the user by id and then just take the registeredevents
-    paddingVertical: 72,
-  },
-  header: {
-    textTransform: 'uppercase',
-    fontStyle: 'italic',
-    fontWeight: 700,
-    fontSize: 32,
-  }
-});
 
 export default function HomeScreen() {
   return (
